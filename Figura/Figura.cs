ab48504bf5a265f59f9fa04a9fb9b5c4992986e6
khinto <bachana.khintibidze.1@btu.edu.ge>
2020-10-04 23:26:49 -0700

    Figura

---
+++ Figura/Figura.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figura
+{
+    class Figura
+    {
+        int gverdi1, gverdi2, gverdi3, perimetri;
+        double fartobi;
+
+        public Figura(int gverdi1)
+        {
+            this.gverdi1 = gverdi1;
+            perimetri = this.gverdi1 * 4;
+            fartobi = this.gverdi1 * this.gverdi1;
+        }
+
+
+        public Figura(int gverdi1, int gverdi2)
+        {
+            this.gverdi1 = gverdi1;
+            this.gverdi2 = gverdi2;
+            perimetri = (this.gverdi1 + this.gverdi2);
+            fartobi = this.gverdi1 * this.gverdi2;
+        }
+
+        public Figura(int gverdi1, int gverdi2, int gverdi3)
+        {
+            this.gverdi1 = gverdi1;
+            this.gverdi2 = gverdi2;
+            this.gverdi3 = gverdi3;
+            perimetri = this.gverdi1 + this.gverdi2 + this.gverdi3;
+            fartobi = this.gverdi1 * this.gverdi2 / 2;
+
+        }
+
+       
+
+
+        public int Perimetri()
+        {
+            return perimetri;
+
+        }
+
+        public double Fartobi()
+        {
+            return fartobi;
+
+        }
+
+        
+    }
+}
---
+++ Figura/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Figura
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int gverdi1, gverdi2, gverdi3, perim_kvadratis, perim_mart, perim_sam;
+            double  fartobi, fartobi_kvadratis, fartobi_sam, fartobi_mart;
+            gverdi1 = int.Parse(textBox1.Text);
+            gverdi2 = int.Parse(textBox2.Text);
+            gverdi3 = int.Parse(textBox3.Text);
+            
+
+
+            Figura martkutxedi = new Figura(gverdi1, gverdi2);
+            Figura kvadrati = new Figura(gverdi1);
+            
+            Figura samkutxedi = new Figura(gverdi1, gverdi2, gverdi3);
+
+            perim_kvadratis = kvadrati.Perimetri();
+            perim_sam = samkutxedi.Perimetri();
+            perim_mart = martkutxedi.Perimetri();
+            fartobi_kvadratis = kvadrati.Fartobi();
+            fartobi_sam = samkutxedi.Fartobi();
+            fartobi_mart = martkutxedi.Fartobi();
+
+
+            label1.Text = "კვადრატის პერიმეტრი " + perim_kvadratis.ToString() + " კვადრატის ფართობი  " + fartobi_kvadratis.ToString();
+
+            label2.Text = "სამკუთხედის პერიმეტრი " + perim_sam.ToString() + " სამკუთხედის ფართობი " + fartobi_sam.ToString();
+
+            label3.Text = "მართკუთხედის პერიმეტრი " + perim_mart.ToString()  +" მართკუთხედის ფართობი " + fartobi_mart.ToString();
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
